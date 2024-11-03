675883dfe409b6c217d4eacc158e996ccdeb8c08
MUHAMMAD HAIKAL BIN MOHD TAHAR <muhammadhaikal.binmohdtahar@etiqa.com.my>
2024-11-03 09:59:48 +0800

    intro bento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import Footer from "../components/_layout/footer";
-import Header from "@/components/_layout/header";
-import TimePickerDialog from "@/components/_time-picker/time-picker-dialog";
-import { Button } from "@/components/ui/button";
-import { Dumbbell, ChevronLeft } from "lucide-react";
-import { title } from "process";
+import { Accessibility, ChartNoAxesCombined, CircleUserRound, Dumbbell, LineChart, User } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
@@ -19,39 +15,47 @@ export default function Home() {
         </h2>
       </div>
 
-      <main className="w-full max-w-5xl">
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-          <div
-            onClick={() => router.push("/workouts")}
-            className="bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer hover:shadow-lg p-4"
-          >
+      <main className="flex-grow w-full max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-3 gap-2">
+        <div
+          onClick={() => router.push("/workouts")}
+          className="bg-white border border-gray-200 rounded-lg col-span-2 shadow-md cursor-pointer hover:shadow-lg p-4"
+        >
+          <div className="flex gap-1 items-center">
+            <Accessibility className="h-5 w-5 text-gray-800" />
             <h3 className="text-lg font-semibold text-gray-800">Workouts</h3>
-            <p className="text-sm text-gray-600">
-              Explore various workout plans for different fitness goals.
-            </p>
           </div>
+          <p className="text-sm text-gray-600">
+            Explore various workout plans for different fitness goals.
+          </p>
+        </div>
 
-          <div
-            onClick={() => router.push("/progress")}
-            className="bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer hover:shadow-lg p-4"
-          >
-            <h3 className="text-lg font-semibold text-gray-800">Progress Tracking</h3>
-            <p className="text-sm text-gray-600">
-              Track your fitness journey with detailed workout logs.
-            </p>
+        <div
+          onClick={() => router.push("/progress")}
+          className="bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer hover:shadow-lg p-4"
+        >
+          <div className="flex gap-1 items-center">
+            <ChartNoAxesCombined className="h-5 w-5 text-gray-800" />
+            <h3 className="text-lg font-semibold text-gray-800">Progress</h3>
           </div>
+          <p className="text-sm text-gray-600">
+            Track your fitness journey with detailed workout logs.
+          </p>
+        </div>
 
-          <div
-            onClick={() => router.push("/profile")}
-            className="bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer hover:shadow-lg p-4"
-          >
+        <div
+          onClick={() => router.push("/profile")}
+          className="bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer hover:shadow-lg p-4"
+        >
+          <div className="flex gap-1 items-center">
+            <CircleUserRound className="h-5 w-5 text-gray-800" />
             <h3 className="text-lg font-semibold text-gray-800">Profile</h3>
-            <p className="text-sm text-gray-600">
-              Customize your settings and view personal stats.
-            </p>
           </div>
+          <p className="text-sm text-gray-600">
+            Customize your settings and view personal stats.
+          </p>
         </div>
       </main>
+
       <Footer />
     </div>
   );
