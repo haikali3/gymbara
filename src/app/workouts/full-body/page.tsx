e0c331b79c42bd2ec45e10e28aef2bc2b5d09a4a
MUHAMMAD HAIKAL BIN MOHD TAHAR <muhammadhaikal.binmohdtahar@etiqa.com.my>
2024-11-05 01:43:53 +0800

    add skeleton

--- src/app/workouts/full-body/page.tsx
+++ src/app/workouts/full-body/page.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import { ExerciseDetails } from "@/app/types/type";
 import { fetchWorkoutDetails } from "@/utils/api";
 import { Button } from "@/components/ui/button";
+import ExerciseCardSkeleton from "@/components/exercise-card-skeleton";
 
 function FullBodyWorkoutPage() {
 
@@ -15,12 +16,12 @@ function FullBodyWorkoutPage() {
   });
 
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error loading data</div>;
   return (
     <div className="min-h-screen bg-gray-50 p-2 pt-4 pb-4 flex flex-col">
       <Header title={"full body"} />
       <div className="grid gap-2 w-full grid-cols-1 md:grid-cols-1">
+        {isLoading && <ExerciseCardSkeleton />}
+        {isError && <div className="p-4 bg-red-100 text-red-700 rounded">Error loading data</div>}
         {data?.map((exercise: ExerciseDetails, index: number) => (
           <ExerciseCard
             key={index}
