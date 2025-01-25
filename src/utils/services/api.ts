d4812bb75bdcc73748abd9b0be5ba23737a0ec80
MUHAMMAD HAIKAL BIN MOHD TAHAR <muhammadhaikal.binmohdtahar@etiqa.com.my>
2025-01-25 22:18:46 +0800

    add cookies to all endpoint

--- src/utils/services/api.ts
+++ src/utils/services/api.ts
@@ -1,15 +1,19 @@
-
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function fetchWorkoutSections() {
-  const response = await fetch(`${BASE_URL}/workout-sections`);
+  const response = await fetch(`${BASE_URL}/workout-sections`, {
+    credentials: 'include', // Include cookies with the request
+  });
   if (!response.ok) throw new Error('Failed to fetch workout sections');
   return await response.json();
 }
 
 export async function fetchWorkoutList(workoutSectionId: number) {
   const response = await fetch(
-    `${BASE_URL}/workout-sections/list?workout_section_id=${workoutSectionId}`
+    `${BASE_URL}/workout-sections/list?workout_section_id=${workoutSectionId}`,
+    {
+      credentials: 'include', // Include cookies with the request
+    }
   );
   if (!response.ok) throw new Error('Failed to fetch workout list');
   return await response.json();
@@ -17,7 +21,10 @@ export async function fetchWorkoutList(workoutSectionId: number) {
 
 export async function fetchWorkoutDetails(workoutSectionId: number) {
   const response = await fetch(
-    `${BASE_URL}/workout-sections/details?workout_section_id=${workoutSectionId}`
+    `${BASE_URL}/workout-sections/details?workout_section_id=${workoutSectionId}`,
+    {
+      credentials: 'include', // Include cookies with the request
+    }
   );
   if (!response.ok) throw new Error('Failed to fetch workout details');
   return await response.json();
@@ -28,7 +35,7 @@ export async function fetchUserDetails() {
     // browser sends cookies with req
     const response = await fetch(`${BASE_URL}/api/user-info`, {
       method: 'GET',
-      credentials: 'include',
+      credentials: 'include', // Include cookies with the request
     });
     if (response.status === 401) {
       throw new Error('Unauthorized: Please log in');
