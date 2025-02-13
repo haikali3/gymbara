53517016eb3d5cd1da44d591f84f9c26a5294d3e
Haikal Tahar <manfdvcL9@gmail.com>
2025-02-13 22:55:19 +0800

    feat: implement workout submission functionality in FullBodyWorkoutPage

--- src/app/workouts/full-body/page.tsx
+++ src/app/workouts/full-body/page.tsx
@@ -4,16 +4,33 @@ import Header from "../../../components/_layout/header";
 import Footer from "../../../components/_layout/footer";
 import { useQuery } from "@tanstack/react-query";
 import { ExerciseDetails } from "@/app/types/type";
-import { fetchWorkoutDetails } from "@/utils/services/api";
+import {
+  fetchWorkoutDetails,
+  submitUserExerciseDetails,
+} from "@/utils/services/api";
 import { Button } from "@/components/ui/button";
 import ExerciseCardSkeleton from "@/components/_exercise-card/exercise-card-skeleton";
 import ExerciseCardError from "@/components/_exercise-card/exercise-card-error";
+import { useWorkoutStore } from "@/stores/useWorkoutStore";
 
-function FullBodyWorkoutPage() {
+export default function FullBodyWorkoutPage() {
   const { data, isLoading, isError, refetch } = useQuery<ExerciseDetails[]>({
     queryKey: ["workoutSections", 1],
     queryFn: () => fetchWorkoutDetails(1), //TODO: make it dynamic for 1,2,3
   });
+
+  // it works!!!! but find a way for zustand to reset/create/delete workout
+  const handleSubmitWorkout = async () => {
+    const { section_id, exercises } = useWorkoutStore.getState();
+    const exerciseArray = Object.values(exercises).flat();
+
+    try {
+      const result = await submitUserExerciseDetails(section_id, exerciseArray);
+      console.log("Workout submitted successfully: ", result);
+    } catch (error) {
+      console.error("Error submitting workout:", error);
+    }
+  };
 
   return (
     <div className="min-h-screen bg-gray-50 p-2 pt-4 pb-4 flex flex-col">
@@ -27,10 +44,8 @@ function FullBodyWorkoutPage() {
       </div>
       <div className="pt-2"></div>
       {/* TODO: Add submit function */}
-      <Button>Finish Workout</Button>
+      <Button onClick={handleSubmitWorkout}>Finish Workout</Button>
       <Footer />
     </div>
   );
 }
-
-export default FullBodyWorkoutPage;
