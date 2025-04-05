e93e93d1a84071c22535b461d67957988aa76bfb
Haikal Tahar <manfdvcL9@gmail.com>
2025-04-05 19:12:25 +0800

    feat: enhance Hero section with updated text, styling, and icon for better engagement

--- src/components/_landing-page/hero-section.tsx
+++ src/components/_landing-page/hero-section.tsx
@@ -1,6 +1,7 @@
 import PageWrapper from "@/components/_layout/page-wrapper";
 import { Button } from "@/components/ui/button";
 import { Typography } from "@/components/ui/typography";
+import { ArrowUpRight } from "lucide-react";
 import React from "react";
 
 export default function HeroSection() {
@@ -10,7 +11,7 @@ export default function HeroSection() {
         <div className="container mx-auto">
           {/* Main Hero Content */}
           <div className="text-center">
-            <div className="mx-auto flex h-7 w-fit items-center rounded-full border border-neutral-200 bg-white px-4 text-xs text-neutral-800">
+            <div className="mx-auto flex h-7 w-fit items-center rounded-full border border-neutral-200 bg-white px-4 text-xs text-neutral-800 mb-2">
               <Typography variant="small">
                 LIMITED TIME OFFER: 30% OFF PREMIUM PLAN
               </Typography>
@@ -23,18 +24,19 @@ export default function HeroSection() {
 
             <Typography
               variant="lead"
-              className="max-w-2xl mx-auto text-gray-600 mb-8"
+              className="max-w-2xl mx-auto text-gray-600 mb-8 mt-3 text-pretty text-lg"
             >
-              10,000+ fitness enthusiasts have transformed their bodies with
-              Gymbara.
+              Be one of the first to experience the future of fitness tracking.
               <br />
-              Join them today with our risk-free trial.
+              Start your today and take the first step towards your fitness
+              goals!
             </Typography>
 
             <div className="flex flex-col sm:flex-row justify-center gap-4 w-full max-w-md mx-auto">
               {/* <Button> */}
               <Button className="bg-black text-white hover:bg-black/90 px-6 py-6 rounded-md text-base w-full shadow-none">
                 Start free 14-day trial
+                <ArrowUpRight />
               </Button>
             </div>
 
@@ -42,7 +44,7 @@ export default function HeroSection() {
               variant="small"
               className="mt-6 text-gray-600 flex items-center justify-center gap-2"
             >
-              <span className="w-2 h-2 bg-green-400 rounded-full inline-block"></span>
+              <span className="w-2 h-2 bg-green-400 rounded-full inline-block animate-pulse"></span>
               No credit card required
             </Typography>
 
