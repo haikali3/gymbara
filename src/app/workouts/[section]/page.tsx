080cd0b09b8d284f9df62cb65926900ac491fa62
Haikal Tahar <manfdvcL9@gmail.com>
2025-04-30 23:36:51 +0800

    fix: wrap exercises data in an object for ExerciseForm component

--- src/app/workouts/[section]/page.tsx
+++ src/app/workouts/[section]/page.tsx
@@ -48,7 +48,7 @@ export default function DynamicWorkoutPage() {
             statusCode={(isExercisesError as any)?.status}
           />
         )}
-        {exercises && <ExerciseForm exercises={exercises} />}
+        {exercises && <ExerciseForm exercises={{ data: exercises }} />}
       </div>
       <Footer />
     </PageWrapper>
